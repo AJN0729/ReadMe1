9e6da9480c48d1e4ff2b9ad1e4d61a01ba4b0c7b
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-10-22 08:38:18 -0500

    Initial Commit

---
+++ ReadMe1/ReadMe1App.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMe1App.swift
+//  ReadMe1
+//
+//  Created by AMStudent on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMe1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
